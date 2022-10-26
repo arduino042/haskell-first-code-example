1ee972598d980842556fc3a2ef50eb2d9cb4b60e
Ǥ ષ φ α <113263510+arduino042@users.noreply.github.com>
2022-10-26 17:32:05 -0300

    first commit

---
+++ first-haskell-program.hs
@@ -0,0 +1,22 @@
+doubleMe x = x + x
+
+
+doubleUs x y = doubleMe x + doubleMe y
+
+
+sum2numbersWithDoubleMin x y = (if x < y then doubleMe x else doubleMe y) + max x y
+
+
+sum2numbersWithDoubleMax x y = (if x > y then doubleMe x else doubleMe y) + min x y
+
+
+
+main = do
+
+	print (doubleMe 2)
+
+	print (doubleUs 2 3)
+
+	print (sum2numbersWithDoubleMin 4 5)
+ 
+	print (sum2numbersWithDoubleMax 5 6)
